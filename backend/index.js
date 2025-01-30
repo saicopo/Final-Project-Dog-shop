93437e33c5fee542c77139c228e8c2a644400a61
Carmine Berardi <carmineopo@libero.it>
2025-01-30 18:33:55 +0100

    configurazione server

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/databse.js";
+
+// import { router as userRouter } from "./routes/routes.js";
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+connectDB();
+
+const port = 3001;
+
+server.get("/", (req, res) => {
+  res.send("Backend in funzione");
+});
+
+// server.use("/api/dogs", dogsRouter);
+
+
+server.listen(port, () => {
+  console.log(`Server in funzione sulla porta ${port}`);
+});
